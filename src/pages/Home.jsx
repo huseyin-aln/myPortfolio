5b4898e3130e3ee8a9f1549b8681571555698e7b
Hüseyin <101873227+huseyin-aln@users.noreply.github.com>
2022-12-08 18:28:04 +0300

    added bgImage

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,14 +1,17 @@
 const Home = () => {
   return (
-    <div className="home d-flex justify-content-center align-items-center flex-column">
-      <div className="container m-auto col-9 homeContainer">
-        <h2 className=" fullName">Hüseyin Arslan</h2>
-        <h2 className=" frondEnd">I'm a Front-End Developer</h2>
-        <h5 className="desc">
-          I’m a Frond-End Developer specializing in building exceptional digital
-          experiences. Currently, I’m focused on building responsive web
-          applications.
-        </h5>
+    <div className="home-container d-flex justify-content-center align-items-center m-auto">
+      <div className="bg-image"></div>
+      <div className="home d-flex justify-content-center align-items-center flex-column">
+        <div className="container m-auto col-9 home-text">
+          <h2 className=" fullName">Hüseyin Arslan</h2>
+          <h2 className=" frondEnd">I'm a Front-End Developer</h2>
+          <h5 className="desc">
+            I’m a Frond-End Developer specializing in building exceptional
+            digital experiences. Currently, I’m focused on building responsive
+            web applications.
+          </h5>
+        </div>
       </div>
     </div>
   );
