348dad354f8b685466e7bb9831ae772d49c8e55c
Hüseyin <101873227+huseyin-aln@users.noreply.github.com>
2022-12-08 14:00:09 +0300

    fixed skills

--- src/pages/Skills.jsx
+++ src/pages/Skills.jsx
@@ -1,101 +1,99 @@
 const Skills = () => {
   return (
-    <>
-      <div className=" d-flex align-items-center justify-content-center mt-md-5 skills-main-div">
-        <article className="skillDiv row container mt-md-2  m-auto col-8 gap-2 d-flex align-items-center justify-content-center">
-          <div className="skill d-flex flex-column col-sm-6 col-md-4 col-lg-3 align-items-center justify-content-between py-1">
-            <div className="image d-flex align-items-center justify-content-center">
-              <img src="images/html.png" alt="html" />
-            </div>
-            <div className="title text-light fs-5">HTML</div>
+    <div className=" d-flex align-items-center justify-content-center mt-md-5 skills-main-div">
+      <article className="skillDiv row container mt-md-2  m-auto col-8 gap-2 d-flex align-items-center justify-content-center">
+        <div className="skill d-flex flex-column col-sm-6 col-md-4 col-lg-2 align-items-center justify-content-between py-1">
+          <div className="image d-flex align-items-center justify-content-center">
+            <img src="images/html.png" alt="html" />
           </div>
+          <div className="title text-light fs-6">HTML</div>
+        </div>
 
-          <div className="skill d-flex flex-column col-sm-6 col-md-4 col-lg-3 align-items-center justify-content-between py-1">
-            <div className="image d-flex align-items-center justify-content-center">
-              <img src="images/css.png" alt="css" />
-            </div>
-            <div className="title text-light fs-5">CSS</div>
+        <div className="skill d-flex flex-column col-sm-6 col-md-4 col-lg-2 align-items-center justify-content-between py-1">
+          <div className="image d-flex align-items-center justify-content-center">
+            <img src="images/css.png" alt="css" />
           </div>
+          <div className="title text-light fs-6">CSS</div>
+        </div>
 
-          <div className="skill d-flex flex-column col-sm-6 col-md-4 col-lg-3 align-items-center justify-content-between py-1">
-            <div className="image d-flex align-items-center justify-content-center">
-              <img src="images/js.png" alt="js" />
-            </div>
-            <div className="title text-light fs-5">JAVASCRIPT</div>
+        <div className="skill d-flex flex-column col-sm-6 col-md-4 col-lg-2 align-items-center justify-content-between py-1">
+          <div className="image d-flex align-items-center justify-content-center">
+            <img src="images/js.png" alt="js" />
           </div>
+          <div className="title text-light fs-6">JAVASCRIPT</div>
+        </div>
 
-          <div className="skill d-flex flex-column col-sm-6 col-md-4 col-lg-3 align-items-center justify-content-between py-1">
-            <div className="image d-flex align-items-center justify-content-center">
-              <img src="images/react.png" alt="react" />
-            </div>
-            <div className="title text-light fs-5">REACT</div>
+        <div className="skill d-flex flex-column col-sm-6 col-md-4 col-lg-2 align-items-center justify-content-between py-1">
+          <div className="image d-flex align-items-center justify-content-center">
+            <img src="images/react.png" alt="react" />
           </div>
+          <div className="title text-light fs-6">REACT</div>
+        </div>
 
-          <div className="skill d-flex flex-column col-sm-6 col-md-4 col-lg-3 align-items-center justify-content-between py-1">
-            <div className="image d-flex align-items-center justify-content-center">
-              <img src="images/django.png" alt="django" />
-            </div>
-            <div className="title text-light fs-5">DJANGO</div>
+        <div className="skill d-flex flex-column col-sm-6 col-md-4 col-lg-2 align-items-center justify-content-between py-1">
+          <div className="image d-flex align-items-center justify-content-center">
+            <img src="images/django.png" alt="django" />
           </div>
+          <div className="title text-light fs-6">DJANGO</div>
+        </div>
 
-          <div className="skill d-flex flex-column col-sm-6 col-md-4 col-lg-3 align-items-center justify-content-between py-1">
-            <div className="image d-flex align-items-center justify-content-center">
-              <img src="images/python.png" alt="django" />
-            </div>
-            <div className="title text-light fs-5">PYTHON</div>
+        <div className="skill d-flex flex-column col-sm-6 col-md-4 col-lg-2 align-items-center justify-content-between py-1">
+          <div className="image d-flex align-items-center justify-content-center">
+            <img src="images/python.png" alt="python" />
           </div>
+          <div className="title text-light fs-6">PYTHON</div>
+        </div>
 
-          <div className="skill d-flex flex-column col-sm-6 col-md-4 col-lg-3 align-items-center justify-content-between py-1">
-            <div className="image d-flex align-items-center justify-content-center">
-              <img src="images/bootstrap5.png" alt="bootstrap" />
-            </div>
-            <div className="title text-light fs-5">BOOTSTRAP</div>
+        <div className="skill d-flex flex-column col-sm-6 col-md-4 col-lg-2 align-items-center justify-content-between py-1">
+          <div className="image d-flex align-items-center justify-content-center">
+            <img src="images/bootstrap5.png" alt="bootstrap" />
           </div>
+          <div className="title text-light fs-6">BOOTSTRAP</div>
+        </div>
 
-          <div className="skill d-flex flex-column col-sm-6 col-md-4 col-lg-3 align-items-center justify-content-between py-1">
-            <div className="image d-flex align-items-center justify-content-center">
-              <img src="images/materialui.png" alt="mui" />
-            </div>
-            <div className="title text-light fs-5">MUI</div>
+        <div className="skill d-flex flex-column col-sm-6 col-md-4 col-lg-2 align-items-center justify-content-between py-1">
+          <div className="image d-flex align-items-center justify-content-center">
+            <img src="images/materialui.png" alt="mui" />
           </div>
+          <div className="title text-light fs-6">MUI</div>
+        </div>
 
-          <div className="skill d-flex flex-column col-sm-6 col-md-4 col-lg-3 align-items-center justify-content-between py-1">
-            <div className="image d-flex align-items-center justify-content-center">
-              <img src="images/sass.png" alt="sass" />
-            </div>
-            <div className="title text-light fs-5">SASS</div>
+        <div className="skill d-flex flex-column col-sm-6 col-md-4 col-lg-2 align-items-center justify-content-between py-1">
+          <div className="image d-flex align-items-center justify-content-center">
+            <img src="images/sass.png" alt="sass" />
           </div>
+          <div className="title text-light fs-6">SASS</div>
+        </div>
 
-          <div className="skill d-flex flex-column col-sm-6 col-md-4 col-lg-3 align-items-center justify-content-between py-1">
-            <div className="image d-flex align-items-center justify-content-center">
-              <img src="images/styled-component.png" alt="ts" />
-            </div>
-            <div className="title text-light fs-5">STYLED-COMPONENTS</div>
+        <div className="skill d-flex flex-column col-sm-6 col-md-4 col-lg-2 align-items-center justify-content-between py-1">
+          <div className="image d-flex align-items-center justify-content-center">
+            <img src="images/styled-component.png" alt="ts" />
           </div>
+          <div className="title text-light fs-6 styled">STYLED-COMPONENTS</div>
+        </div>
 
-          <div className="skill git d-flex flex-column col-sm-6 col-md-4 col-lg-3 align-items-center justify-content-between py-1">
-            <div className="image d-flex align-items-center justify-content-center">
-              <img src="images/git.png" alt="git" />
-            </div>
-            <div className="title text-light fs-5">GIT</div>
+        <div className="skill git d-flex flex-column col-sm-6 col-md-4 col-lg-2 align-items-center justify-content-between py-1">
+          <div className="image d-flex align-items-center justify-content-center">
+            <img src="images/git.png" alt="git" />
           </div>
+          <div className="title text-light fs-6">GIT</div>
+        </div>
 
-          <div className="skill d-flex flex-column col-sm-6 col-md-4 col-lg-3 align-items-center justify-content-between py-1">
-            <div className="image d-flex align-items-center justify-content-center">
-              <img src="images/github.png" alt="github" />
-            </div>
-            <div className="title text-light fs-5">GITHUB</div>
+        <div className="skill d-flex flex-column col-sm-6 col-md-4 col-lg-2 align-items-center justify-content-between py-1">
+          <div className="image d-flex align-items-center justify-content-center">
+            <img src="images/github.png" alt="github" />
           </div>
+          <div className="title text-light fs-6">GITHUB</div>
+        </div>
 
-          <div className="skill d-flex flex-column col-sm-6 col-md-4 col-lg-3 align-items-center justify-content-between py-1">
-            <div className="image d-flex align-items-center justify-content-center">
-              <img src="images/jira.png" alt="jira" />
-            </div>
-            <div className="title text-light fs-5">JIRA</div>
+        <div className="skill d-flex flex-column col-sm-6 col-md-4 col-lg-2 align-items-center justify-content-between py-1">
+          <div className="image d-flex align-items-center justify-content-center">
+            <img src="images/jira.png" alt="jira" />
           </div>
-        </article>
-      </div>
-    </>
+          <div className="title text-light fs-6">JIRA</div>
+        </div>
+      </article>
+    </div>
   );
 };
 
