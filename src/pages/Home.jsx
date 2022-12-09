d3dcb09b9b2f0839ea7afc6be5dd182d5cfb4c8b
Hüseyin <101873227+huseyin-aln@users.noreply.github.com>
2022-12-09 16:19:30 +0300

    finished style

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,16 +1,12 @@
 const Home = () => {
   return (
-    <div className="home-container d-flex justify-content-center align-items-center m-auto">
-      <div className="bg-image"></div>
-      <div className="home d-flex justify-content-center align-items-center flex-column">
-        <div className="container m-auto col-9 home-text">
-          <h2 className=" fullName">Hüseyin Arslan</h2>
-          <h2 className=" frondEnd">I'm a Front-End Developer</h2>
-          <h5 className="desc">
-            I’m a Frond-End Developer specializing in building exceptional
-            digital experiences. Currently, I’m focused on building responsive
-            web applications.
-          </h5>
+    <div className="home-container d-flex justify-content-center align-items-center">
+      <div className="bg-image">
+        <div className="home d-flex justify-content-center align-items-center flex-column">
+          <div className="container col-9 home-text">
+            <h2 className=" frondEnd">Hi, my name is Hüseyin.</h2>
+            <h5 className="desc">I’m a Frond-End Developer.</h5>
+          </div>
         </div>
       </div>
     </div>
